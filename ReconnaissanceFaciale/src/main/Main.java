66995fe60423908f48c4d258734127b3ba19f499
cytech <chambasmat@cy-tech.fr>
2022-03-26 16:12:02 +0100

    Retour état sans test

--- ReconnaissanceFaciale/src/main/Main.java
+++ ReconnaissanceFaciale/src/main/Main.java
@@ -2,9 +2,6 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("bonjour");
-		System.out.println("test 2");
-		System.out.println("test 3");
-		System.out.println("test 4");
+		
 	}
 }
