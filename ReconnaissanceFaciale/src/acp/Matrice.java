fd010c0499192ea95aa411b48ccfff612713db1d
cytech <sallmariem@cy-tech.fr>
2022-03-27 13:23:58 +0200

    methode pour calculer la matrice de covariance

--- ReconnaissanceFaciale/src/acp/Matrice.java
+++ ReconnaissanceFaciale/src/acp/Matrice.java
@@ -1,14 +1,16 @@
 package acp;
 
+import Jama.Matrix;
+
 public class Matrice {
 	private int n;
 	private int m;
 	private Pixel[][] pixels;
-	
+
 	public Matrice(int n, int m) {
-		this.n=n;
-		this.m=m;
-		this.pixels= new Pixel[this.n][this.m];
+		this.n = n;
+		this.m = m;
+		this.pixels = new Pixel[this.n][this.m];
 	}
 
 	public final int getN() {
@@ -34,8 +36,22 @@ public class Matrice {
 	public final void setPixels(Pixel[][] pixels) {
 		this.pixels = pixels;
 	}
-	
-	
-	
-	
+
+	public Matrix matriceCovariance() {
+		Matrix mCov = new Matrix(this.n, this.n);
+
+		for (int i = 0; i < this.n; i++) {
+			for (int j = 0; j < this.n; j++) {
+				mCov.set(i, j, 0);
+
+				for (int k = 0; k < this.m; k++) {
+					double temp = mCov.get(i, j) + this.pixels[i][k].getIntensite() * this.pixels[j][k].getIntensite();
+					mCov.set(i, j, temp);
+				}
+
+			}
+		}
+		return mCov;
+	}
+
 }
