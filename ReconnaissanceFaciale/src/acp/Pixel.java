6c3c646a71933950c00ca7a1b58a60664544115f
cytech <sallmariem@cy-tech.fr>
2022-03-27 12:50:26 +0200

    classe pixel comme en UML

---
+++ ReconnaissanceFaciale/src/acp/Pixel.java
@@ -0,0 +1,28 @@
+package acp;
+
+public class Pixel {
+	private double intensite;
+	private int positionN;
+	private int positionM;
+	
+	public final double getIntensite() {
+		return intensite;
+	}
+	public final void setIntensite(double intensite) {
+		this.intensite = intensite;
+	}
+	public final int getPositionN() {
+		return positionN;
+	}
+	public final void setPositionN(int positionN) {
+		this.positionN = positionN;
+	}
+	public final int getPositionM() {
+		return positionM;
+	}
+	public final void setPositionM(int positionM) {
+		this.positionM = positionM;
+	}
+	
+	
+}
