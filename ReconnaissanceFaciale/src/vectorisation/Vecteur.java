c15b55d3a179d9b77d632cf15e47781068bf4ca1
cytech <chambasmat@cy-tech.fr>
2022-04-03 17:56:01 +0200

    Methode niveau de gris, et transformation vecteur en matrice ajoutée, suppression de vectorisation.java (implémentée dans les autres fichiers)

--- ReconnaissanceFaciale/src/vectorisation/Vecteur.java
+++ ReconnaissanceFaciale/src/vectorisation/Vecteur.java
@@ -8,16 +8,21 @@ import java.io.IOException;
 import javax.imageio.ImageIO;*/
 
 import acp.Matrice;
+import acp.Pixel;
 
 public class Vecteur {
-	public double[] v;	
+	public Pixel[] v;	
 	
 	public Vecteur() {
-		v = new double[10000];
+		v = new Pixel[10000];
 	}
 	
 	public int getNbLigne() {
 		return this.v.length;
+	}
+	
+	public Pixel[] getPixels() {
+		return v;
 	}
 	
 	public void transfoVect(Matrice matrice) {
@@ -28,11 +33,23 @@ public class Vecteur {
          while (indice < n*n) {
          	for (int i = 0; i < n; i++) {
 	            	for (int j = 0; j < n; j++) {
-	            		this.v[indice] = matrice.getPixels()[i][j].getIntensite();
+	            		this.v[indice].setIntensite(matrice.getPixels()[i][j].getIntensite());
 	            		indice++;
 	            	}
 	            }
          }
 	 }
 	
+	
+	public Matrice transfoMat() {
+		Matrice matrice = new Matrice(100, 100);
+		 int k = 0;
+		 for (int i = 0; i<matrice.getN(); i++) {
+			 for (int j = 0; j < matrice.getM(); j++) {
+				 matrice.getPixels()[i][j].setIntensite(this.v[k].getIntensite());
+				 k++;
+			 }
+		 }
+		 return matrice;
+	}
 }
