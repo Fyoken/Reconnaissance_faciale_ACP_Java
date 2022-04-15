40db77cf02517eb92d7822933478d03d03c47b5f
cytech <chambasmat@cy-tech.fr>
2022-04-15 15:53:33 +0200

    import inutil enlevé

--- ReconnaissanceFaciale/src/main/Main.java
+++ ReconnaissanceFaciale/src/main/Main.java
@@ -13,7 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.Chart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.layout.StackPane;
