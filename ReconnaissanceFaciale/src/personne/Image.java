92b358af9a35d6405ff8e88402fcd7f343d13a95
cytech <chambasmat@cy-tech.fr>
2022-05-02 18:05:48 +0200

    AJout commentaire classe image

--- ReconnaissanceFaciale/src/personne/Image.java
+++ ReconnaissanceFaciale/src/personne/Image.java
@@ -17,34 +17,62 @@ public class Image {
 	protected String nomImage;
 	protected Matrice photo;
 
-	//Constructeur
+	/*
+	 * Objectif : Construire une nouvelle image avec le nom donnée
+	 * @param : nomImage (la localisation de l'image depuis le dossier src/)
+	 */
 	public Image(String nomImage) {
 		this.nomImage = nomImage;
 		this.photo =  new Matrice(50,50);
 		this.changeSize();
 		this.transformationNiveauGris();
 	}
+	
+	/*
+	 * Objectif : Construire une nouvelle image avec le nom et la matrice associée à l'image donnée
+	 * @param : nomImage (la localisation de l'image depuis le dossie src/)
+	 * 			mat		 (la matrice associée à l'image)
+	 */
 	public Image(String nomImage, Matrice mat) {
 		this.nomImage = nomImage;
 		this.photo = mat;
 	}
 	
-	//Get et set du nom de l'image (= le lien d'où est stocké l'image)
+	/*
+	 * Objectif : Obtenir l'image (le lien ou est stockée l'image)
+	 * @return : nomImage (le lien où est stockée l'image)
+	 */
 	public String getNomImage() {
 		return nomImage;
 	}
 
+	/*
+	 * Objectif : Modifier l'image (le lien ou est stockée l'image)
+	 * @param : nomImage (le nouveau lien où est stockée l'image)
+	 */
 	public void setNomImage(String nomImage) {
 		this.nomImage = nomImage;
 	}	
 	
+	/*
+	 * Objectif : Obtenir la matrice liée à l'image
+	 * @return : photo (la matrice liée à l'image)
+	 */
 	public final Matrice getPhoto() {
 		return photo;
 	}
+	
+	/*
+	 * Objectif : Modifier la matrice liée à l'image
+	 * @param : photo (la nouvelle matrice liée à l'image)
+	 */
 	public final void setPhoto(Matrice photo) {
 		this.photo = photo;
 	}
-	//Méthode pour redimensionner la taille d'une image et la passer en niveau de gris
+	
+	/*
+	 * Objectif : Redimensionner la taille d'une image et la passer en niveau de gris
+	 */
 	public void changeSize() {
 		String outImg;
 		int w, h;
@@ -81,7 +109,9 @@ public class Image {
 		}
 	}
 	
-	//Méthode pour recupérer une image
+	/*
+	 * Objectif : Récupérer une image
+	 */
 	public void transformationNiveauGris() {
 		// lit l'image d'entrée
 		File f = new File(this.getNomImage());
@@ -101,7 +131,10 @@ public class Image {
 		}	      
 	}
 
-	
+	/*
+	 * Objectif : Retourner le nom de l'image (le lien où l'image est stockée) sous forme de string (texte)
+	 * @return : this.getNomImages() (le string du lien où l'image est stockée)
+	 */
 	@Override
 	public String toString() {
 		return(this.getNomImage());
